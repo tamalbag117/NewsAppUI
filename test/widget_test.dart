1d97ff2030bdadbb12e29ffe532a28b12f1b0e2c
tamalbag117 <tamalbag107@gmail.com>
2022-01-14 20:30:42 +0530

    second commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:newsdart/main.dart';
+import 'package:dartnews/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
