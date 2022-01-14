09d5f997062fdab16a115220faa5c66ef9b918a6
tamalbag117 <tamalbag107@gmail.com>
2022-01-14 20:19:57 +0530

    initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:newsdart/screens/child_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  PageController _pageController = PageController(
+    initialPage: 0,
+  );
+  int currentIndex = 0;
+
+  Widget childWidget = ChildWidget(
+    number: AvailableNumber.First,
+  );
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.grey[500],
+        currentIndex: currentIndex,
+        onTap: (value) {
+          currentIndex = value;
+          _pageController.animateToPage(
+            value,
+            duration: Duration(milliseconds: 200),
+            curve: Curves.linear,
+          );
+
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            //abel: "First",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.trending_up),
+            //title: Text("Second"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            //title: Text("Third"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+           // lable:"Third",
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (page) {
+          setState(() {
+            currentIndex = page;
+          });
+        },
+        children: <Widget>[
+          ChildWidget(number: AvailableNumber.First),
+          ChildWidget(number: AvailableNumber.Second),
+          ChildWidget(number: AvailableNumber.Third),
+          ChildWidget(number: AvailableNumber.Third)
+        ],
+      ),
+    );
+  }
+}
